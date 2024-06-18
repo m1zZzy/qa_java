3d1f9157b104e6be541ddd0beaa19c63fa7946c0
Ruslan Golyautdinov <golyautdinov.r@yandex.ru>
2024-06-18 20:18:46 +0300

    Sprint 6

--- src/main/java/com/example/LionAlex.java
+++ src/main/java/com/example/LionAlex.java
@@ -1,2 +1,27 @@
-package com.example;public class LionAlex {
+package com.example;
+
+import java.util.List;
+
+public class LionAlex extends Lion {
+
+    public LionAlex(Feline feline) throws Exception {
+        super(feline, "Самец");
+    }
+
+    public List<String> getFriends() {
+        return List.of(
+                "Зебра Марти",
+                "Бегемотиха Глория",
+                "Жираф Мелман"
+        );
+    }
+
+    public String getPlaceOfLiving() {
+        return "Нью-Йоркский зоопарк";
+    }
+
+    @Override
+    public int getKittens() {
+        return 0;
+    }
 }
